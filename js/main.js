b6e5f57675869672030a55514bd810e13979b651
Maksim Zozulya <soundmax15041987@gmail.com>
2025-06-25 14:32:25 +0300

    implemented saving of user data of the task list in local storage

--- js/main.js
+++ js/main.js
@@ -4,19 +4,25 @@ const input = document.querySelector('[data-text-field]');
 const addToDoBtn = document.querySelector('[data-add-todo-btn]');
 const container = document.querySelector('[data-todo-container]');
 
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todos')) || [];
 
 addToDoBtn.addEventListener('click', () => {
     if (input.value.trim()) {  
         todoList.push(input.value); 
         input.value = '';
-        
-        container.innerHTML = '';
-        todoList.forEach((todo) => {
-            const todoElement = document.createElement('div');
-            todoElement.textContent = todo;
-            container.append(todoElement);    
-        });
 
+        localStorage.setItem('todos', JSON.stringify(todoList))
+        render();
     }
 });
+
+const render = () => {
+    container.innerHTML = '';
+    todoList.forEach((todo) => {
+    const todoElement = document.createElement('div');
+    todoElement.textContent = todo;
+    container.append(todoElement); 
+    });
+}
+
+render();
