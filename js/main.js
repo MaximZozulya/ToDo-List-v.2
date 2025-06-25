953f44f8ed744cfd59603901d267893ec7911c2e
Maksim Zozulya <soundmax15041987@gmail.com>
2025-06-25 14:10:33 +0300

    reworked the basic logic and added the functionality of adding a task to an array

--- js/main.js
+++ js/main.js
@@ -4,11 +4,19 @@ const input = document.querySelector('[data-text-field]');
 const addToDoBtn = document.querySelector('[data-add-todo-btn]');
 const container = document.querySelector('[data-todo-container]');
 
+const todoList = [];
+
 addToDoBtn.addEventListener('click', () => {
-    if (input.value.trim()) {        
-        const todo = document.createElement('div');
-        todo.textContent = input.value;
-        container.append(todo);
+    if (input.value.trim()) {  
+        todoList.push(input.value); 
         input.value = '';
+        
+        container.innerHTML = '';
+        todoList.forEach((todo) => {
+            const todoElement = document.createElement('div');
+            todoElement.textContent = todo;
+            container.append(todoElement);    
+        });
+
     }
 });
